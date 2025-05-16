e008c8f610377548015227d59c466e119aa61f93
pauloHOAlmeida <paulo.almeida@epiousion.com.br>
2025-05-15 22:27:53 -0300

    :sparkles: feat: added responses

--- src/main/java/com/know_me/know_me/features/relationship/entrypoints/http/dtos/response/FindPersonWithSameInterestsResponseDto.java
+++ src/main/java/com/know_me/know_me/features/relationship/entrypoints/http/dtos/response/FindPersonWithSameInterestsResponseDto.java
@@ -2,4 +2,4 @@ package com.know_me.know_me.features.relationship.entrypoints.http.dtos.response
 
 import java.util.Set;
 
-public record FindPeopleWithSameInterestsResponseDto (String id, String name, Set<String> interests) {}
+public record FindPersonWithSameInterestsResponseDto(String id, String name, Set<String> interests) {}
